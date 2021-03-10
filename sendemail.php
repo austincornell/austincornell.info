27627089af4d9db32d0d923fca3be66d9c204b75
Austin Cornell <racornel@iu.edu>
2021-03-10 03:38:35 -0500

    added re-captcha because bots were being terrible.

--- sendemail.php
+++ sendemail.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Austin Cornell | About</title>
-
+<title>Austin Cornell | Contact</title>
+<?php  session_start(); ?>
 <!--Import Google Icon Font-->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cantarell">
@@ -52,9 +52,9 @@
 <div class="section white">
   <div class="row container ">
     <div class="col s12 m12">
-      <h2 class="header">Thanks for reaching out, <?php echo $_POST["fullname"];?>.</h2>
+      <h2 class="header">Thanks for reaching out, <?php echo $_SESSION["fullname"];?>.</h2>
 		<hr/>
-      <p class="grey-text text-darken-3">I'll try to get back to you ASAP. The email I'll reach out to is <b><?php echo $_POST["email"]; ?></b>. Below is the full copy of your responses:</p>
+      <p class="grey-text text-darken-3">I'll try to get back to you ASAP. The email I'll reach out to is <b><?php echo $_SESSION["email"]; ?></b>. Below is the full copy of your responses:</p>
       <hr/>
 
     </div>
@@ -67,7 +67,7 @@
               <h5 class="header">Name </h5>
           </div>
           <div class="col s12 m6">
-              <p><?php echo $_POST["fullname"]; ?></p>
+              <p><?php echo $_SESSION["fullname"]; ?></p>
           </div>
 
       </div>
@@ -76,7 +76,7 @@
               <h5 class="header">Email </h5>
           </div>
           <div class="col s12 m6">
-              <p><?php echo $_POST["email"]; ?></p>
+              <p><?php echo $_SESSION["email"]; ?></p>
           </div>
 
       </div>
@@ -85,7 +85,7 @@
               <h5 class="header">Company </h5>
           </div>
           <div class="col s12 m6">
-              <p><?php echo $_POST["company"]; ?></p>
+              <p><?php echo $_SESSION["company"]; ?></p>
           </div>
 
       </div>
@@ -94,7 +94,7 @@
               <h5 class="header">Response </h5>
           </div>
           <div class="col s12 m6">
-              <p><?php echo $_POST["response"]; ?></p>
+              <p><?php echo $_SESSION["response"]; ?></p>
           </div>
 
       </div>
@@ -104,25 +104,71 @@
       <img width="200px" src="images/greencheck.gif" alt="greencheck" class="circle"/>
     </center>
     <?php
+   
+
     $today = date("F j, Y, g:i a");
-    $to = $_POST["email"];
-    $name = $_POST["fullname"];
-    $company = $_POST["company"];
-    $response = $_POST["response"];
+    $to = $_SESSION["email"];
+    $name = $_SESSION["fullname"];
+    $company = $_SESSION["company"];
+    $response = $_SESSION["response"];
 
 
-    $subject = "Thanks for contacting me, " . $name;
-    $headers = "From: Austin Cornell <www-data@test-instance-1.us-central1-a.c.inspiring-ring-281403.internal>" . PHP_EOL . "Reply-To: Austin Cornell <www-data@test-instance-1.us-central1-a.c.inspiring-ring-281403.internal>" . PHP_EOL . "X-Mailer: PHP/" . phpversion() . PHP_EOL;
-    $headers .= "MIME-Version: 1.0" . PHP_EOL;
-    $headers .= "Content-type:text/html;charset=UTF-8" . PHP.EOL;
-    $message = "
+    
+    // $headers = "From: Austin Cornell <www-data@test-instance-1.us-central1-a.c.inspiring-ring-281403.internal>" . PHP_EOL . "Reply-To: Austin Cornell <www-data@test-instance-1.us-central1-a.c.inspiring-ring-281403.internal>" . PHP_EOL . "X-Mailer: PHP/" . phpversion() . PHP_EOL;
+    // $headers .= "MIME-Version: 1.0" . PHP_EOL;
+    // $headers .= "Content-type:text/html;charset=UTF-8" . PHP.EOL
+
+
+
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    // If necessary, modify the path in the require statement below to refer to the
+    // location of your Composer autoload.php file.
+    require 'vendor/autoload.php';
+
+    // Replace sender@example.com with your "From" address.
+    // This address must be verified with Amazon SES.
+    $sender = 'richardmcornell@gmail.com';
+    $senderName = 'Your Website';
+
+    // Replace recipient@example.com with a "To" address. If your account
+    // is still in the sandbox, this address must be verified.
+    $recipient = 'richardmcornell@gmail.com';
+
+    // Replace smtp_username with your Amazon SES SMTP user name.
+    $usernameSmtp = 'AKIAS3UE4XGHHWLPG46Q';
+
+    // Replace smtp_password with your Amazon SES SMTP password.
+    $passwordSmtp = 'BMdJ2IKeJvjCd6mB1bLtQayom0BmRSzyOLfAc4HOYgNB';
+
+    // Specify a configuration set. If you do not want to use a configuration
+    // set, comment or remove the next line.
+    //$configurationSet = 'ConfigSet';
+
+    // If you're using Amazon SES in a region other than US West (Oregon),
+    // replace email-smtp.us-east-2.amazonaws.com with the Amazon SES SMTP
+    // endpoint in the appropriate region.
+    $host = 'email-smtp.us-east-2.amazonaws.com';
+    $port = 587;
+
+    // The subject line of the email
+    $subject = "{$name} has emailed you.";
+
+    // The plain-text body of the email
+    $bodyText =  "{$name},{$to},{$company},{$response},{$today}";
+
+    // The HTML-formatted body of the email
+    $bodyHtml = "
                 <html>
                 <head>
                 <title>Thanks for reaching out...</title>
                 </head>
                 <body>
                 <h3 style='color: darkred;'> Thanks for reaching out. </h3>
-                <p style='color: grey;'><i>Here is a copy of your responses. Please do not reply to this email: </i></p>
+                <p style='color: grey;'><i>Here is a copy of their responses. Please do not reply to this email: </i></p>
                 <h4>Name</h4>
                 <p style='color: grey;'>{$name}</p>
                 <hr/>
@@ -135,13 +181,44 @@
                 <h4>Response</h4>
                 <p style='color: grey;'>{$response}</p>
                 <br/><hr/>
-                <p style='color: grey;'><i>This email is automated. I will attempt to respond as quickly as possible. Generated on {$today} </i></p>
+                <p style='color: grey;'><i>This email is automated. Generated on {$today} </i></p>
                 </body>
                 </html>
                 ";
 
-    mail($to, $subject, $message, $headers);
-    mail("richardmcornell@gmail.com","{$name} has emailed you.", $message, $headers)
+    $mail = new PHPMailer(true);
+
+    try {
+      // Specify the SMTP settings.
+      $mail->isSMTP();
+      $mail->setFrom($sender, $senderName);
+      $mail->Username   = $usernameSmtp;
+      $mail->Password   = $passwordSmtp;
+      $mail->Host       = $host;
+      $mail->Port       = $port;
+      $mail->SMTPAuth   = true;
+      $mail->SMTPSecure = 'tls';
+      $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+
+      // Specify the message recipients.
+      $mail->addAddress($recipient);
+      // You can also add CC, BCC, and additional To recipients here.
+
+      // Specify the content of the message.
+      $mail->isHTML(true);
+      $mail->Subject    = $subject;
+      $mail->Body       = $bodyHtml;
+      $mail->AltBody    = $bodyText;
+      $mail->Send();
+    } catch (phpmailerException $e) {
+      echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+    } catch (Exception $e) {
+      echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+    }
+    session_unset();
+    session_destroy();
+    
+
 
        ?>
 
---
+++ verify_captcha.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+  if(!empty($_POST['g-recaptcha-response']))
+  {
+        $secret = '6Le18dEZAAAAAMkq771Zl2UHWQsuhz14eh1al5jl';
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success){
+            $_SESSION["email"] = $_POST["email"];
+            $_SESSION["fullname"] = $_POST["fullname"];
+            $_SESSION["company"] = $_POST["company"];
+            $_SESSION["response"] = $_POST["response"];  
+            header("Location: sendemail.php");
+            exit();
+        } else { 
+            header("Location: contact.html");
+            exit();
+        }
+      
+   }else{
+        header("Location: contact.html");
+        exit();
+   }
+?>
